c9ff6ba7ee5ca0380fe2edbce6be25fb3cf5fc90
ckwatt11 <ckwatt11@gmail.com>
2023-01-03 11:09:28 -0500

    update README.md

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'grbaqxf3&*^rohdhgtp01aq%3d!1&%d8pga^@a!p_15y367o@m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'base',
+    'livereload',
 ]
 
 MIDDLEWARE = [
